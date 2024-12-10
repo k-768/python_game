e2466be5caf52a4576fca62e38aa8065f0aebe40
k-768 <r20034@st.omu.ac.jp>
2024-12-10 10:22:03 +0900

    feat: create map02.py and map03.py

---
+++ map03.py
@@ -0,0 +1,71 @@
+import os
+import tkinter as tk
+from PIL import Image,ImageTk
+
+#>>ディレクトリ>>
+cwd = os.getcwd()
+
+#>>マップ設定>>
+CHIP_SIZE = 64 #マップチップの大きさ
+X_MAPSIZE = 20 #マップのx方向タイル数
+Y_MAPSIZE = 10 #マップのy方向タイル数
+
+#>>ウィンドウ、キャンバス>>
+CANVAS_WIDTH = CHIP_SIZE * X_MAPSIZE #キャンバス幅
+CANVAS_HEIGHT = CHIP_SIZE * Y_MAPSIZE #キャンバス高さ
+MARGINE_X = 2 #マージン
+MARGINE_Y = 2 #マージン
+CANVAS_SIZE = f"{CANVAS_WIDTH+MARGINE_X}x{CANVAS_HEIGHT+MARGINE_Y}"#キャンバスサイズ
+
+#ウィンドウ設置
+root = tk.Tk()
+root.title("マップ表示")
+root.geometry(CANVAS_SIZE)
+
+#キャンバス設置
+canvas = tk.Canvas(root,width = CANVAS_WIDTH,height = CANVAS_HEIGHT,bg = "skyblue")
+canvas.pack()
+
+
+#>>マップチップ>>
+#マップチップを1枚の画像に並べたマップシートを読み込む
+MAP_SHEET = Image.open(cwd+"/img/sheet1.png")
+
+#読み込んだ画像から縦横何枚ずつチップがあるか求める
+CHIP_X = MAP_SHEET.width // CHIP_SIZE
+CHIP_Y = MAP_SHEET.height // CHIP_SIZE
+
+#マップシートをマップチップに分割し配列に格納する
+MAP_CHIP = []
+
+for y in range(CHIP_Y):
+    for x in range(CHIP_X):
+        image = ImageTk.PhotoImage(MAP_SHEET.crop((
+            CHIP_SIZE*x , 
+            CHIP_SIZE*y , 
+            CHIP_SIZE*(x+1) , 
+            CHIP_SIZE*(y+1)
+            )))
+        MAP_CHIP.append(image)
+
+
+#>>マップデータ>>
+MAP_DATA = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 3, 3, 3, 3],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 3, 3, 3],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 3, 3, 3, 3],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3],
+    [1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+    [1, 1, 1, 1, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+    [2, 2, 1, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+    [3, 3, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+    [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+    [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+]
+
+
+for y in range(Y_MAPSIZE):
+    for x in range(X_MAPSIZE):
+        canvas.create_image(x*CHIP_SIZE,y*CHIP_SIZE,image =MAP_CHIP[MAP_DATA[y][x]] ,tag=f"chip{x},{y}",anchor=tk.NW)
+
+root.mainloop()
